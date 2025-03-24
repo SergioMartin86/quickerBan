1c836014083e4e8a47289048ee74516c4e42c5ac
SergioMartin86 <eienburuu@gmail.com>
2025-03-24 20:42:54 +0100

    Getting static map

--- source/core/room.hpp
+++ source/core/room.hpp
@@ -65,21 +65,66 @@ class Room
      for (uint8_t j = 0; j < _width; j++)
      _tiles[getIndex(i,j)] = itemType::floor;
     
-     // Parsing from input
+    // Parsing from input
+    _boxCount = 0;
+    _goalCount = 0;
     for (uint8_t i = 0; i < _height; i++)
     {
         const auto& row = rowSequence[i];
         for (uint8_t j = 0; j < row.size(); j++)
         {
             if (row[j] == ' ' || row[j] == '-' || row[j] == '_') _tiles[getIndex(i,j)] = itemType::floor;
-            if (row[j] == '.') _tiles[getIndex(i,j)] = itemType::goal;
-            if (row[j] == '*' || row[j] == 'B') _tiles[getIndex(i,j)] = itemType::box_on_goal;
-            if (row[j] == 'b' || row[j] == '$') _tiles[getIndex(i,j)] = itemType::box;
-            if (row[j] == 'P' || row[j] == '+') _tiles[getIndex(i,j)] = itemType::pusher_on_goal;
+            if (row[j] == '.') { _tiles[getIndex(i,j)] = itemType::goal; _goalCount++; }
+            if (row[j] == '*' || row[j] == 'B') { _tiles[getIndex(i,j)] = itemType::box_on_goal; _boxCount++; _goalCount++; }
+            if (row[j] == 'b' || row[j] == '$') { _tiles[getIndex(i,j)] = itemType::box; _boxCount++;  }
+            if (row[j] == 'P' || row[j] == '+') { _tiles[getIndex(i,j)] = itemType::pusher_on_goal; _goalCount++; }
             if (row[j] == 'p' || row[j] == '@') _tiles[getIndex(i,j)] = itemType::pusher;
             if (row[j] == '#') _tiles[getIndex(i,j)] = itemType::wall;
         }
     }
+
+    // Sanity check: boxes = goals
+    if (_boxCount != _goalCount) JAFFAR_THROW_LOGIC("Number of boxes (%lu) is not equal to goals (%lu)", _boxCount, _goalCount);
+
+    // Allocating state space
+    _stateSize = 2 * sizeof(uint8_t) * (1 + _boxCount);
+    _state = (uint8_t*)aligned_alloc(pageSize, _stateSize);
+
+    // Building internal state
+    size_t currentBox = 1;
+    for (uint8_t i = 0; i < _height; i++)
+     for (uint8_t j = 0; j < _width; j++)
+     {
+        if (_tiles[getIndex(i,j)] == itemType::pusher)
+        { 
+            _state[0] = i;
+            _state[1] = j;
+            _tiles[getIndex(i,j)] = floor;
+        }
+        
+        if (_tiles[getIndex(i,j)] == itemType::pusher_on_goal)
+        {
+             _state[0] = i;
+             _state[1] = j;
+             _tiles[getIndex(i,j)] = goal;
+        }
+
+        if (_tiles[getIndex(i,j)] == itemType::box)
+        {
+             _state[currentBox * 2 + 0] = i;
+             _state[currentBox * 2 + 1] = j;
+             _tiles[getIndex(i,j)] = floor;
+             currentBox++;
+        }
+
+        if (_tiles[getIndex(i,j)] == itemType::box_on_goal)
+        {
+             _state[currentBox * 2 + 0] = i;
+             _state[currentBox * 2 + 1] = j;
+             _tiles[getIndex(i,j)] = goal;
+             currentBox++;
+        }
+     }
   }
 
   private:
@@ -89,7 +134,11 @@ class Room
   uint8_t* _tiles = nullptr;
   uint8_t _width = 0;
   uint8_t _height = 0;
+
+  uint8_t* _state = nullptr;
+  size_t _stateSize;
   size_t _boxCount = 0;
+  size_t _goalCount = 0;
 
 };
 
